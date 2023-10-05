4a678bbf42297d74299eeb3f75083030fe55d3dc
David <grayd500@gmail.com>
2023-10-04 23:13:32 -0500

    moved format to all ESM.  Updated index.mjs, package.json and shape.js

---
+++ index.mjs
@@ -0,0 +1,59 @@
+import inquirer from 'inquirer';
+import fs from 'fs';
+import { Circle, Triangle, Square } from './lib/shapes.js';
+
+
+const questions = [
+    {
+      type: 'input',
+      name: 'logoText',
+      message: 'Enter the text for your logo./n Maximum: 3 letters',
+    },
+    {
+      type: 'input',
+      name: 'textColor',
+      message: `Enter a text color keyword or a hexadecimal number for the logo's test color`,
+    },
+    {
+        type: 'input',
+        name: 'logoColour',
+        message: `Please enter a colour keyword or a hexadecimal number for the logo's background colour`,
+      },
+      {
+        type: 'list',
+        name: 'logoShape',
+        message: `Select a logo shape`,
+        choices: ['triangle', 'circle', 'square'],
+      },
+];
+
+function init() {
+    inquirer.prompt(questions)
+    .then((answers) => {
+        let shapeInstance;
+        switch (answers.logoShape) {
+            case 'circle':
+                shapeInstance = new Circle(answers.logoText, answers.textColor, answers.logoColour);
+                break;
+            case 'triangle':
+                shapeInstance = new Triangle(answers.logoText, answers.textColor, answers.logoColour);
+                break;
+            case 'square':
+                shapeInstance = new Square(answers.logoText, answers.textColor, answers.logoColour);
+                break;
+            default:
+                console.log('Invalid shape selected.');
+                return;
+        }
+
+        const svgContent = shapeInstance.render();
+
+        fs.writeFile('logo.svg', svgContent, (err) => {
+            if (err) throw err;
+            console.log('Generated logo.svg!');
+        });
+    });
+}
+      
+      // Function call to initialize app
+      init();
